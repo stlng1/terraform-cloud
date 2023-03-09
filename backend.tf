800d7d48ad6ea9da281fb6a233ed8144066f9422
Femi Solagbade <femi@strangenig.com>
2023-03-09 14:09:48 +0100

    uncomment roles commented earlier

--- backend.tf
+++ backend.tf
@@ -28,15 +28,15 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
   }
 }
 
-# resource "aws_dynamodb_table" "terraform_locks" {
-#   name         = "terraform-locks"
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "LockID"
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
 
 /*# Backend must remain commented until the Bucket
  and the DynamoDB table are created. 
--- modules/VPC/roles.tf
+++ modules/VPC/roles.tf
@@ -1,54 +1,54 @@
-#Create AssumeRole
-# resource "aws_iam_role" "ec2_instance_role" {
-#   name = "ec2_instance_role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Sid    = ""
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
+# Create AssumeRole
+resource "aws_iam_role" "ec2_instance_role" {
+  name = "ec2_instance_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 
-#   tags = merge(
-#     var.tag,
-#     {
-#       Name = format("%s-aws_assume_role", var.project_name)
-#     },
-#   )
-# }
+  tags = merge(
+    var.tag,
+    {
+      Name = format("%s-aws_assume_role", var.project_name)
+    },
+  )
+}
 
-#Create IAM policy for this role
-# resource "aws_iam_policy" "policy" {
-#   name        = "ec2_instance_policy"
-#   description = "A test policy"
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = [
-#           "ec2:Describe*",
-#         ]
-#         Effect   = "Allow"
-#         Resource = "*"
-#       },
-#     ]
+# Create IAM policy for this role
+resource "aws_iam_policy" "policy" {
+  name        = "ec2_instance_policy"
+  description = "A test policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
 
-#   })
+  })
 
-#   tags = merge(
-#     var.tag,
-#     {
-#       Name = format("%s-aws_assume_policy", var.project_name)
-#     },
-#   )
+  tags = merge(
+    var.tag,
+    {
+      Name = format("%s-aws_assume_policy", var.project_name)
+    },
+  )
 
-# }
+}
 
 #Attach policy to role 
 resource "aws_iam_role_policy_attachment" "test-attach" {
